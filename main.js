8cd21f597f81b6db86431bb156cc97a38064b030
jessachalla <jessa.challa@gvmc.org>
2019-03-04 11:41:37 -0500

    added notes

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 (function(){
+    //initial positions and direction
     let position = 0;
     let direction = "right";
 
+    //moveForward:takes a distance parameter.
+    //Move the character forward
+    //(based on the direction they are facing) 
+    //the specified distance.
+    //the specified distance is the argument where the function is being called at the very bottom
     function moveForward(distance) {
         if (direction === "right") {
             position = position + distance;
@@ -11,6 +17,11 @@
         console.log(`${position} to the ${direction}`);
     }
 
+    //moveBackward:takes a distance parameter.
+    //Move the character backward
+    //(basedon the direction they are facing) t
+    //he specified distance.
+    //the specified distance is the argument where the function is being called at the very bottom
     function moveBackward(distance) {
         if (direction === "right") {
             position = position - distance;
@@ -20,6 +31,7 @@
         console.log(`${position} to the ${direction}`);
     }
 
+    //turnAround:reverse the direction the character is facing.
     function turnAround() {
         if (direction === "right") {
             direction = "left";
@@ -29,6 +41,7 @@
         }
     }
 
+    //printLocation:logs the current position to the console.
     function printLocation() {
         console.log(`you are at this postiosn ${position}`);
     }
